ab3eac345db6fa3a15aaa8b8a472b6d89d8282b1
TheDooft <thedooft@gmail.com>
2012-03-08 14:39:14 +0100

    .

--- src/gui/ParsingTab.java
+++ src/gui/ParsingTab.java
@@ -1,6 +1,7 @@
 package gui;
 
-import javax.swing.BoxLayout;
+import java.awt.FlowLayout;
+
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -11,7 +12,7 @@ public class ParsingTab extends JPanel {
 	private static final long serialVersionUID = -8617942858027114616L;
 
 	public ParsingTab() {
-		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		setLayout(new FlowLayout(FlowLayout.LEFT));
 		JTextField textFieldFileName = new JTextField("./sample/WoWCombatLog.txt",30);
 		
 		this.add(textFieldFileName);
